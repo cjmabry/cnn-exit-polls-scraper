eed83a937b915d39cc076f662db184dd68f508dd
cjmabry <cjmab28@gmail.com>
2016-03-20 16:21:20 -0500

    Added census regions for better analysis

--- exit_polls.py
+++ exit_polls.py
@@ -3,9 +3,87 @@ from pprint import pprint
 
 party = 'Dem'
 candidates = ['Clinton', 'Sanders']
-# party = 'Rep'
-# candidates = ['Trump', 'Cruz', 'Kasich']
 
+# manually group states by region for better analysis
+# http://www2.census.gov/geo/pdfs/reference/GARM/Ch6GARM.pdf
+regions = {
+    'South' : {
+        'South Atlantic' : [
+            'WV',
+            'MD',
+            'DE',
+            'DC',
+            'VA',
+            'NC',
+            'SC',
+            'GA',
+            'FL'
+        ],
+        'East South Central': [
+            'KY',
+            'TN',
+            'MS',
+            'AL'
+        ],
+        'West South Central': [
+            'OK',
+            'AR',
+            'LA',
+            'TX'
+        ]
+    },
+    'West' : {
+        'Mountain': [
+            'MT',
+            'ID',
+            'WY',
+            'NV',
+            'UT',
+            'CO',
+            'AZ',
+            'NM'
+        ],
+        'Pacific': [
+            'AK',
+            'WA',
+            'OR',
+            'HI'
+        ]
+    },
+    'Midwest' : {
+        'West North Central' : [
+            'ND',
+            'SD',
+            'NE',
+            'KS',
+            'MN',
+            'IA',
+            'MO'
+        ],
+        'East North Central' : [
+            'WI',
+            'IL',
+            'MI',
+            'IN',
+            'OH'
+        ]
+    },
+    'Northeast' : {
+        'Middle Atlantic' : [
+            'PA',
+            'NY',
+            'NJ'
+        ],
+        'New England' : [
+            'ME',
+            'VT',
+            'NH',
+            'MA',
+            'CT',
+            'RI'
+        ]
+    }
+}
 
 r = requests.get('http://data.cnn.com/elections/2016/config.json')
 
@@ -19,28 +97,34 @@ for poll in exitpollhub:
     elif party == 'Rep':
         urls = ['Rfull','Sfull'] # IA and NV use Sfull
 
+    state = poll['statecode']
+
+
     for url in urls:
-        r = requests.get('http://data.cnn.com/ELECTION/2016primary/' + poll['statecode'] + '/xpoll/'+ url + '.json')
+        r = requests.get('http://data.cnn.com/ELECTION/2016primary/' + state + '/xpoll/'+ url + '.json')
 
         if r.status_code == 200:
+            for region, subregions in regions.iteritems():
+                for subregion, states in subregions.iteritems():
+                    if state in states:
+                        subregion = subregion
+                        region = region
 
             if poll['party'] == party:
-                filename = 'data/' + party + '/' + poll['statecode'] + '-' + party +'.csv'
+                filename = 'data/' + party + '/' + state + '-' + party +'.csv'
                 data = r.json()
                 d = {}
 
                 with open(filename, 'wb') as f:
                     csv_file = csv.writer(f)
-                    csv_file.writerow(['Question','pollname','Answer','Percentage of Electorate','Clinton','Sanders'])
+                    csv_file.writerow(['question','pollname','state','region','subregion','answer','percentage of electorate','clinton','sanders'])
 
                     for item in data['polls']:
 
-                        # dictionary for debugging
                         d[item['question']] = {}
 
                         for answer in item['answers']:
 
-                            # dictionary for debugging
                             d[item['question']][answer['answer']] = {}
                             d[item['question']][answer['answer']]['Percent of Electorate'] = answer['pct']
 
@@ -56,4 +140,4 @@ for poll in exitpollhub:
 
                                 d[item['question']][answer['answer']][candidate] = a['pct']
 
-                            csv_file.writerow([item['question'],item['pollname'],answer['answer'],answer['pct'],d[item['question']][answer['answer']][candidates[0]],d[item['question']][answer['answer']][candidates[1]]])
+                            csv_file.writerow([item['question'],item['pollname'],state,region,subregion,answer['answer'],answer['pct'],d[item['question']][answer['answer']][candidates[0]],d[item['question']][answer['answer']][candidates[1]]])
