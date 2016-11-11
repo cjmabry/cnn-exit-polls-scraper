1d76fc0c1e458967ea1e3fc2f74589d06c4d2d6a
Aaron Bannin <aaronbannin@gmail.com>
2016-11-10 19:21:47 -0800

    copy into exit_polls from temp table

--- pg_importer.py
+++ pg_importer.py
@@ -30,13 +30,18 @@ def main():
                 print("filename: " + fileDir + '/' + year + '/' + filename)
                 cursor = pgConn.cursor()
                 cursor.execute("delete from temp;")
+                pgConn.commit()
                 importFile = open(fileDir + '/' + year + '/' + filename, "r+")
                 cursor.copy_from(importFile, 'temp', sep = '\t', null="")
                 pgConn.commit()
 
                 # merge temp table into primary table
-                # cursor.execute("delete from exit_polls where year = " + year + " and state = " + state + ";")
-                # cursor.execute("insert into exit_polls select * from temp;")
+                state = filename.split('.')[0]
+                cursor.execute("delete from exit_polls where year = " + str(year) + " and state = '''" + state + "''';")
+                pgConn.commit()
+                # select * from temp where state = 'AL' and year = '2012
+                cursor.execute("insert into exit_polls select * from temp;")
+                pgConn.commit()
 
 
 
