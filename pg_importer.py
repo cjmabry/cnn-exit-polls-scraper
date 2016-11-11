15cf542983fff268bb3ab647329e2c22c240bffd
Aaron Bannin <aaronbannin@gmail.com>
2016-11-10 17:46:55 -0800

    stub out copy func

--- pg_importer.py
+++ pg_importer.py
@@ -2,6 +2,7 @@ import psycopg2
 import os
 
 
+
 def main():
 
     fileDir = './data/general'
@@ -14,6 +15,7 @@ def main():
 
     try:
         pgConn = psycopg2.connect(**pgParams)
+        # pgConn.set_client_encoding('UTF8')
         print('db connection successful')
     except psycopg2.Error as e:
         print('db connection failure')
@@ -22,7 +24,19 @@ def main():
         
     for year in ['2012', '2016']:
         for filename in os.listdir(fileDir + '/' + year):
-            copyIntoPG(filename)
+
+            # first import file into temp table
+            # copyIntoPG(filename)
+            cursor = pgConn.cursor()
+            cursor.execute("delete from temp;")
+            importFile = open(fileDir + '/' + year + '/' + filename, "r+", encoding='utf-8')
+            cursor.copy_from(importFile, 'temp', sep = ',')
+            cursor.commit()
+
+            # merge temp table into primary table
+            # cursor.execute("delete from exit_polls where year = " + year + " and state = " + state + ";")
+            # cursor.execute("insert into exit_polls select * from temp;")
+
             
             
         
