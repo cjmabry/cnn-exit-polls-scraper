55b04df0d781a5a813fbee61743c288be756995c
Aaron Bannin <aaronbannin@gmail.com>
2016-11-14 11:26:43 -0800

    2016 state results working

--- county_results_importer.py
+++ county_results_importer.py
@@ -1,170 +1,100 @@
-import requests, json
-import unicodecsv as csv
-from pprint import pprint
+import os
 import psycopg2
+import json
 
-# config for primaries
-party = 'Dem'
-candidates = ['Clinton', 'Sanders']
 
-# config for generals
-parties = ['Dem', 'Rep']
+fileDir = './data/counties'
 
-# manually group states by region for better analysis
-# http://www2.census.gov/geo/pdfs/reference/GARM/Ch6GARM.pdf
-regions = {
-    'South' : {
-        'South Atlantic' : [
-            'WV',
-            'MD',
-            'DE',
-            'DC',
-            'VA',
-            'NC',
-            'SC',
-            'GA',
-            'FL'
-        ],
-        'East South Central': [
-            'KY',
-            'TN',
-            'MS',
-            'AL'
-        ],
-        'West South Central': [
-            'OK',
-            'AR',
-            'LA',
-            'TX'
-        ]
-    },
-    'West' : {
-        'Mountain': [
-            'MT',
-            'ID',
-            'WY',
-            'NV',
-            'UT',
-            'CO',
-            'AZ',
-            'NM'
-        ],
-        'Pacific': [
-            'AK',
-            'WA',
-            'OR',
-            'HI'
-        ]
-    },
-    'Midwest' : {
-        'West North Central' : [
-            'ND',
-            'SD',
-            'NE',
-            'KS',
-            'MN',
-            'IA',
-            'MO'
-        ],
-        'East North Central' : [
-            'WI',
-            'IL',
-            'MI',
-            'IN',
-            'OH'
-        ]
-    },
-    'Northeast' : {
-        'Middle Atlantic' : [
-            'PA',
-            'NY',
-            'NJ'
-        ],
-        'New England' : [
-            'ME',
-            'VT',
-            'NH',
-            'MA',
-            'CT',
-            'RI'
-        ]
-    }
+pgParams = {
+    'dbname': 'abannin',
+    'user': 'abannin',
+    'password': ''
 }
 
+try:
+    pgConn = psycopg2.connect(**pgParams)
+    print('db connection successful')
+except psycopg2.Error as e:
+    print('db connection failure')
+    print(e)
 
-def getCountyDataForStateAndYear(state):
-    year = 2016
-    # example urls
-    # P_county.json = president
-    # S_county.json = senate
-    # http://data.cnn.com/ELECTION/2016/AZ/county/P_county.json
-    # http://data.cnn.com/ELECTION/2016/NV/county/P_county.json
-    print('getCountyDataForState: ' + state + ' year = ' + str(year))
-    r = requests.get('http://data.cnn.com/ELECTION/' + str(year) + '/' + state + '/county/P_county.json')
-    # print(r.json())
-    # saveToCSV(r, state, year)
-
-def saveToCSV(request, state, year):
-    print('saveToCSV begin for state = ' + state + ' year = ' + str(year))
-
-    state_subregion = ''
-    state_region = ''
-    if response.status_code == 200:
-        for region, subregions in regions.iteritems():
-            for subregion, states in subregions.iteritems():
-                if state in states:
-                    state_subregion = subregion
-                    state_region = region
-
-        filename = 'data/general/' + str(year) + '/' + state + '.csv'
-        data = response.json()
-        states = {}
-        counties = {}
-
-        with open(filename, 'wb') as f:
-            csv_file = csv.writer(f, delimiter='\t', dialect='excel', encoding='utf-8')
+def main():
+    # for filename in os.listdir(fileDir + '/' + '2016'):
+    #     print("filename = "+ filename)
+    #     if filename.endswith('.json'):
+    #         with open(fileDir + '/' + '2016/' + filename) as jsonData:
+    #             state = json.load(jsonData)
+    #             extractData2016(state)
+    with open(fileDir + '/' + '2012/AZ.json') as jsonData:
+        state = json.load(jsonData)
+        extractData2016(state)
 
 
-def extractData(resultsJSON):
+def BoolStringToInt(string):
+    if string.lower() == 'false':
+        return 0
+    elif string.lower() == 'true':
+        return 1
+
+
+def extractData2016(stateJSON):
+    print('extractData2016 begin')
     # one record in StateDataByYear
-    raceDict = resultsJSON['race']
+    raceDict = stateJSON['race']
     stateId = raceDict['code']
     stateName = raceDict['state']
-    isKeyRace = raceDict['keyrace']
+    isKeyRace = int(raceDict['keyrace'])
     electoralVotes = raceDict['evotes']
     pollClosedTimestamp = raceDict['pollclose']
     stateCalledByCNNTimestamp = raceDict['calltime']
     percentReporting = raceDict["pctsrep"]
 
+    cursor = pgConn.cursor()
+    statement = 'INSERT into state_data_by_year \
+        (state_id, state_name, year, is_key_race, electoral_votes, poll_closed_ts, state_called_by_cnn_ts, percent_reporting) \
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)' 
+    values = (stateId, stateName, '2016', isKeyRace, electoralVotes, pollClosedTimestamp, stateCalledByCNNTimestamp, percentReporting)
+    cursor.execute(statement, values)
+    # pgConn.commit()
+
+
+
     # many records in CandidateDataByState
-    for candidate in raceDict['candidates']:
+    for candidate in stateJSON['race']['candidates']:
         candidateId = candidate['id']
         lastName = candidate['lname']
         party = candidate['party']
-        isWinner = candidate['winner']
+        isWinner = int(candidate['winner'])
         percentOfVote = candidate['pctDecimal']
         totalVotes = candidate['votes']
         electoralVotesWon = candidate['evotes']
+        cursor = pgConn.cursor()
+        statement = 'INSERT into candidate_data_by_state \
+            (state_id, year, candidate_id, last_name, party, is_winner, percent_of_vote, total_votes, electoral_votes_won) \
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)' 
+        values = (stateId, '2016', candidateId, lastName, party, isWinner, percentOfVote, totalVotes, electoralVotesWon)
+        cursor.execute(statement, values)
 
-    # one record in CountyDataByYear
-    countiesDict = resultsJSON['counties']
-    cnnCountyId = countiesDict['co_id']
-    countyName = countiesDict['name']
-    updatedAtTimestamp = countiesDict['race']['ts']
-    percentReporting = countiesDict['race']['pctsrep']
+    pgConn.commit()
 
-    # many records in CandidateDataByCounty
-    candidateDict = countiesDict['race']['candidates']
-    candidateId = candidateDict['id']
-    lastName = candidateDict['lname']
-    party = candidateDict['party']
-    isWinner = candidateDict['winner']
-    percentOfVote = candidateDict['pctDecimal']
-    totalVotes = candidateDict['votes']
+    # # one record in CountyDataByYear
+    # countiesDict = resultsJSON['counties']
+    # cnnCountyId = countiesDict['co_id']
+    # countyName = countiesDict['name']
+    # updatedAtTimestamp = countiesDict['race']['ts']
+    # percentReporting = countiesDict['race']['pctsrep']
 
-
-
-
+    # # many records in CandidateDataByCounty
+    # candidateDict = countiesDict['race']['candidates']
+    # candidateId = candidateDict['id']
+    # lastName = candidateDict['lname']
+    # party = candidateDict['party']
+    # isWinner = candidateDict['winner']
+    # percentOfVote = candidateDict['pctDecimal']
+    # totalVotes = candidateDict['votes']
 
 
-getCountyDataForStateAndYear('NV')
+main()
+
+
+
